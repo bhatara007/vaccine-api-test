490625dea1170276775d7613b9b53fbb42cf2a0b
bhatara007 <bhatara007@gmail.com>
2021-11-18 11:29:00 +0700

    rename and fix docstring place

--- test_getdata_api.py
+++ test_getdata_api.py
@@ -11,43 +11,43 @@ URL = "https://suchonsite-server.herokuapp.com/people"
 
 class ServiceSiteGetDataByDateAPITest(unittest.TestCase):
 
-    """
-    Test ID: 1
-    Test API endpoint by correct date format with status code
-    """
     def test_get_people_by_date_status_code(self):
+        """
+        Test ID: 1
+        Test API endpoint by correct date format with status code
+        """
         current_day = '20-10-2021' #we only have a data for 20-10-2021
         api = URL + "/by_date/" + current_day
         res = requests.get(api)
         self.assertEqual(200, res.status_code)
     
-    """
-    Test ID: 2
-    Test API endpoint with current day and correct date format we should get the data with the same date that
-    we requested
-    """
     def test_get_people_by_date(self):
+        """
+        Test ID: 2
+        Test API endpoint with current day and correct date format we should get the data with the same date that
+        we requested
+        """
         current_day = '20-10-2021' #we only have a data for 20-10-2021
         api = URL + "/by_date/" + current_day
         res = requests.get(api)
         people = res.json()
         self.assertEqual(people['date'], current_day)
 
-    """
-    Test ID: 3
-    Test API endpoint with wrong date format
-    """
     def test_get_people_by_wrong_date_format_1(self):
+        """
+        Test ID: 3
+        Test API endpoint with wrong date format
+        """
         wrong_current_day = today.strftime('%d/%m/%Y')
         api = URL + "/by_date/" + wrong_current_day
         res = requests.get(api)
         self.assertEqual(404, res.status_code)
     
-    """
-    Test ID: 4
-    Test API response JSON schema structure
-    """
     def test_get_people_schema_structure(self):
+        """
+        Test ID: 4
+        Test API response JSON schema structure
+        """
         schema = {
             "type" : "object",
             "properties" : {
@@ -63,21 +63,21 @@ class ServiceSiteGetDataByDateAPITest(unittest.TestCase):
         #it will throws error exception
         self.assertIsNone(validate(instance=response, schema=schema))
 
-    """
-    Test ID: 5
-    Test API endpoint status code with future day data 
-    """
     def test_get_people_by_future_date(self):
+        """
+        Test ID: 5
+        Test API endpoint status code with future day data 
+        """
         future_date = (today + timedelta(5)).strftime('%d-%m-%Y')
         api = URL + "/by_date/" + future_date
         res = requests.get(api)
         self.assertEqual(202, res.status_code)
     
-    """
-    Test ID: 6
-    Test API response person JSON schema structure
-    """
     def test_get_person_schema_structure(self):
+        """
+        Test ID: 6
+        Test API response person JSON schema structure
+        """
         schema = {
             "type" : "object",
             "properties" : {
@@ -101,58 +101,58 @@ class ServiceSiteGetDataByDateAPITest(unittest.TestCase):
             #it will throws error exception
             self.assertIsNone(validate(instance=person, schema=schema))
     
-    """
-    Test ID: 7
-    Test for check whether response data is JSON or not?
-    """
     def test_check_type_of_response_2(self):
+        """
+        Test ID: 7
+        Test for check whether response data is JSON or not?
+        """
         api = URL + "/by_date/20-10-2021"
         response = requests.get(api)
         self.assertEqual(response.headers.get('content-type'), 'application/json; charset=utf-8')
     
-    """
-    Test ID: 8
-    Test API endpoint with wrong date format
-    """
     def test_get_people_by_wrong_date_format_2(self):
+        """
+        Test ID: 8
+        Test API endpoint with wrong date format
+        """
         wrong_current_day = "10-OCT-2021"
         api = URL + "/by_date/" + wrong_current_day
         res = requests.get(api)
         self.assertEqual(404, res.status_code)
     
-    """
-    Test ID: 9
-    Test API get all data status code
-    """
     def test_get_all_data_status_code(self):
+        """
+        Test ID: 9
+        Test API get all data status code
+        """
         api = URL + "/all/"
         response = requests.get(api)
         self.assertEqual(response.status_code, 200)
     
-    """
-    Test ID: 10
-    Test for check whether response data is JSON or not?
-    """
     def test_check_type_of_response_1(self):
+        """
+        Test ID: 10
+        Test for check whether response data is JSON or not?
+        """
         api = URL + "/all/"
         response = requests.get(api)
         self.assertEqual(response.headers.get('content-type'), 'application/json; charset=utf-8')
     
-    """
-    Test ID: 11
-    Test API to add the existed data to our database status code
-    """
     def test_add_exist_data_to_data_status_code(self):
+        """
+        Test ID: 11
+        Test API to add the existed data to our database status code
+        """
         api = URL + '20-10-2021'
         response = requests.post(api)
         #should return 401 status code
         self.assertEqual(response.status_code, 401)
     
-    """
-    Test ID: 12
-    Test API for fetchinh data failed from goverment 
-    """
     def test_fetch_data_failed_status_code(self):
+        """
+        Test ID: 12
+        Test API for fetchinh data failed from goverment 
+        """
         api = URL + '19-1-1999'
         response = requests.post(api)
         #should return 504 failed status code
